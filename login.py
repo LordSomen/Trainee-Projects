137c12ba0179edf9db5fca88e4911538a9bd6c8b
Mushkan <mushkan.soni@craftvedatechnology.com>
2022-01-10 17:26:55 +0530

    Added registration.py and modified login.py

--- login.py
+++ login.py
@@ -1,1 +1,46 @@
-print("Hello World!")
+import psycopg2
+from passlib.hash import pbkdf2_sha256
+conn = None
+cur = None
+try:
+    conn = psycopg2.connect(
+                host = 'localhost', 
+                database = 'User_N',
+                user = 'postgres',
+                password = 'nameitasyoulike',
+                port = 5432
+                )
+
+    cur = conn.cursor()
+
+
+    print("!!!!   Verifying Credentials   !!!!")
+    #TAKING INPUT FOR VERIFICATION
+    user_email_ver = input('Enter E-mail: ')
+    user_pswd_ver = input('Enter Password: ')
+    if user_email_ver != '' and user_pswd_ver !='':
+        db_check_mail = (user_email_ver,)
+        cur.execute('SELECT user_password FROM userrv WHERE user_email = %s',db_check_mail)
+        db_mail_pswd = cur.fetchone()
+
+        #TO CHECK IF THE USER ID PROVIDED IS IN THE DATA TABLE OR NOT
+        if db_mail_pswd != None:
+
+            verified = pbkdf2_sha256.verify(user_pswd_ver, db_mail_pswd[0])
+            if verified == True:
+                print({'Status Code':200, 'msg': 'Verified Successfully!'})
+            else:
+                print({'Status Code':206, 'msg': 'Incorrect Password!'})
+        else:
+            print({'Status Code':401, 'msg': 'Invalid User!'})
+    else:
+        raise Exception
+    conn.commit()
+except Exception as error:
+    print({'Status Code':500, 'msg': 'Connection Failed!'})
+
+finally:
+    if cur is not None:
+         cur.close()
+    if conn is not None:
+        conn.close()
---
+++ registration.py
@@ -0,0 +1,66 @@
+import psycopg2, uuid, re
+from passlib.hash import pbkdf2_sha256
+conn = None
+cur = None
+try:
+    conn = psycopg2.connect(
+                host = 'localhost', 
+                database = 'User_N',
+                user = 'postgres',
+                password = 'nameitasyoulike',
+                port = 5432
+                )
+
+    cur = conn.cursor()
+
+    #CREATING USERS TABLE
+    create_script = ''' CREATE TABLE IF NOT EXISTS userrv(
+        User_ID uuid PRIMARY KEY,
+        User_email varchar(60) NOT NULL,
+        User_password varchar(100) NOT NULL
+    )
+    '''
+    cur.execute(create_script)
+    
+    cur.execute('''SELECT user_email FROM userrv''')
+    users_list_fetch = cur.fetchall()
+    users_list = [item for t in users_list_fetch for item in t]
+
+    #TO INSERT DATA INTO USERS TABLE
+    no_of_users = int(input('Enter No. Of Users: '))
+    if no_of_users == 0:
+        print({'Status Code':416, 'msg': 'Requested Range Unsatisfiable!'})
+    else:
+        for i in range(no_of_users):
+            user_uid = uuid.uuid4()
+            j =0
+            while j == 0:
+                user_mail = input('Enter Your Mail ID: ')
+                j += 1
+                if user_mail in users_list:
+                    print({'Status Code':409, 'msg': 'Already in Database. Add another Email ID'})
+                    j -= 1
+                elif not re.search('^[a-z0-9]+[\._]?[a-z0-9]+[@]\w+[.]\w{2,3}$', user_mail):
+                    print({'Status Code':406, 'msg': 'Enter Valid eMail!'})
+                    j -= 1
+
+            k = 0
+            while k == 0:
+                user_pswd = input('Enter Password: ')
+                k = 1
+                if re.search('[\s]',user_pswd) or user_pswd == '' :
+                    print({'Status Code':422, 'msg': 'Try another way!'})
+                    k -= 1
+            insert_script = ''' INSERT INTO userrv (user_id, user_email, user_password) VALUES(%s, %s, %s)'''
+            insert_values = (str(user_uid), user_mail, pbkdf2_sha256.hash(user_pswd))
+            cur.execute(insert_script, insert_values)
+            print({'Status Code':200, 'msg': 'Succesfully Done'})
+    conn.commit()
+except Exception as error:
+    print({'Status Code':500, 'msg': 'Something Happened. Try again later.'})
+
+finally:
+    if cur is not None:
+         cur.close()
+    if conn is not None:
+        conn.close()
